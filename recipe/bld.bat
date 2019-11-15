8134abfed549d3fd28c49c279f17e2eb7aed4d3a
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-11-15 17:06:15 +0100

    ..

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,7 @@ cd build
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -D CMAKE_BUILD_TYPE=Release ^
       ..
 
 if errorlevel 1 exit 1
