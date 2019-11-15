642bf4a33201483bdbe4c217d9f594003243daab
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-11-15 13:07:34 +0100

    add win

---
+++ recipe/bld.bat
@@ -0,0 +1,15 @@
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      ..
+
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
