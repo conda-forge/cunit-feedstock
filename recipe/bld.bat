267efad2f76c37a2cf3e7b34968163a21cc4bd77
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-04-21 08:09:22 +0200

    small improvements, build shared on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,17 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" ^
+cmake -G "Ninja" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D BUILD_SHARED_LIBS=ON ^
       ..
 
 if errorlevel 1 exit 1
 
-nmake
+ninja
 if errorlevel 1 exit 1
 
-nmake install
+ninja install
 if errorlevel 1 exit 1 
