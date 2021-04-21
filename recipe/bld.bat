42156b55c83e1fbb10d3426e3dfc54e2db0b116b
Tobias Fischer <info@tobiasfischer.info>
2021-04-21 14:40:50 +1000

    Create bld.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,16 @@
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      ..
+
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1 
